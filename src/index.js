c53a6b372c83912b2244f3a1871393592de22510
Thanh Hùng Lý <thanhhung270502@gmail.com>
2023-09-25 20:24:57 +0700

    Using google-oauth to support login with google

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const route = require('./routes');
+const session = require('express-session');
+const passport = require('passport');
+const passportStrategy = require('./passport');
 
 const app = express();
 const PORT = 3000;
@@ -13,6 +16,17 @@ app.use(
         credentials: true,
     }),
 );
+
+app.use(
+    session({
+        resave: false,
+        saveUninitialized: true,
+        secret: 'SECRET',
+    }),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Routes
 route(app);
