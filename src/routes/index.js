b22150044f35213e5d2b8b881f85099a8ac004e2
Thanh Hùng Lý <thanhhung270502@gmail.com>
2023-09-25 09:27:29 +0700

    User: getAllUsers, getUser, createUser. Session: login. Start project with nodemon

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,10 @@
 const siteRouter = require('./site');
+const sessionsRouter = require('./sessions');
+const usersRouter = require('./users');
 
 function route(app) {
+    app.use('/sessions', sessionsRouter);
+    app.use('/users', usersRouter);
     app.use('/', siteRouter);
 }
 
